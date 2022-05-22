c9f80b380d54db39536ebd6834f8f8a4e1f3f555
OmarOraby <52020494@edu.campus02.at>
2022-05-22 22:51:58 +0200

    Adding dependency for log4j

--- src/main/java/at/campus02/bsd/Main.java
+++ src/main/java/at/campus02/bsd/Main.java
@@ -1,7 +1,12 @@
 package at.campus02.bsd;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 
 public class Main {
     public static Calculator calculator = new Calculator();
+    private static Logger logger = LogManager.getLogger();
+
 
     public static void main(String[] args) {
         calculator.add(5,4);
